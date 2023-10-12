9c3330fb3339236fdec0921047de4f4931484f2e
matrepriya <138755505+matrepriya@users.noreply.github.com>
2023-10-12 18:07:07 +0530

    lenght_breadth.cs         multiplication.cs         squareprograme.cs

---
+++ c#/multiplication.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace multiplication
+{
+    class programe
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3, mul")
+        
+
+          Console.WriteLine("enter num1");
+            Console.WriteLine("num1=" * num2);
+            Console.WriteLine("num2=" * num2);
+            Console.ReadKey();
+
+
+        }
+    }
+}
+
---
+++ c#/squareprograme.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace squareprograme
+{
+    class square
+    {
+        public static void Main()
+        {
+            int num, sqr;
+            Console.WriteLine("enter number");
+            num = Convert.ToInt32(Console.ReadLine());
+            sqr = num * num;
+            Console.WriteLine("square" + sqr);
+            Console.ReadKey();
+        }
+    }
+}
