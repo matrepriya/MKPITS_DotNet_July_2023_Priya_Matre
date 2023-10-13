f8fc53a4f762da3db946a7dc9e6877da7696b81f
matrepriya <138755505+matrepriya@users.noreply.github.com>
2023-10-13 18:18:16 +0530

    accept_three_subject_marks.cs

---
+++ c#/accept_three_subject_marks.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace program
+{
+    class programe
+    {
+        static void Main(string[]args)
+        {
+            int sub1,sub2,sub3,total;
+            float per;
+           
+            string grade = null;
+            Console.WriteLine("enter 3 subject marks");
+            sub1 = Convert.ToInt32(Console.ReadLine());
+            sub2 = Convert.ToInt32(Console.ReadLine());
+            sub3 = Convert.ToInt32(Console.ReadLine());
+            total=sub1 +sub2+sub3;
+            Console.WriteLine("percentage:{0} ", total);
+            per = (total / 300.0f) * 100.0f;
+            Console.WriteLine("percentage : {0} ", per);
+
+            if (per>=75)
+            {
+                grade = "distinction";
+            }
+            else if (per>=60 && per<75)
+            {
+                grade = "first class";
+            }
+            else
+            {
+                grade = "fail";
+            }
+            Console.WriteLine("grade:{0}",grade);
+            Console.ReadKey();
+
+        }
+    }
+}
