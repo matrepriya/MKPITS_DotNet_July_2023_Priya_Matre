e44642c3e4a61ebea3c0436f0cc0fa7fd212acc0
matrepriya <138755505+matrepriya@users.noreply.github.com>
2023-10-13 18:40:49 +0530

    age_candiate_own_vote.cs

---
+++ c#/age_candiate_own_vote.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace voteprograme
+{
+    class program
+    {
+        public static void Main()
+        {
+            int age;
+            Console.WriteLine("enter a age");
+            age = Convert.ToInt32(Console.ReadLine());
+            if (age >== 18)
+            {
+                Console.WriteLine("  eligible for vote ");
+            }
+            else
+            {
+                Console.WriteLine("is not eligible");
+            }
+            Console.ReadKey();
+        }
+    }
+}
