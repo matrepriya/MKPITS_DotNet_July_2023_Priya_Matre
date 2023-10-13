94a5cfb41967044bf8705a2fb49f5bcfafbb9d73
matrepriya <138755505+matrepriya@users.noreply.github.com>
2023-10-13 22:36:35 +0530

    integer_less_than_zero.cs

---
+++ c#/integer_less_than_zero.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace programe
+{
+    class mvalue
+    {
+    public static void Main()
+        {
+            int num;
+            Console.WriteLine("enter test data");
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num > 0)
+            {
+                Console.WriteLine("0");
+            }
+            else
+                Console.WriteLine("-1");
+
+        }
+       
+    }
+}
