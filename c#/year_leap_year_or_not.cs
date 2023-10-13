25caa195ed8ef1982960ee16abdcff4654aa6088
matrepriya <138755505+matrepriya@users.noreply.github.com>
2023-10-13 23:00:33 +0530

    year_leap_year_or_not.cs

---
+++ c#/year_leap_year_or_not.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace leapyearprograme
+{
+    class program
+    {
+        public static void Main()
+        {
+            int num;
+            Console.WriteLine("enter num");
+            num=Convert.ToInt32(Console.ReadLine());
+            if (num%4==0)
+            { 
+            Console.WriteLine("is a leap year");
+            }
+            else
+            {
+                Console.WriteLine("it is not leap year");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
