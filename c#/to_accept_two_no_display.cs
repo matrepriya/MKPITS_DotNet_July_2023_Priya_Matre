77f5fc485d9e60fd53dc1e7b76bc73819d0a5c5e
matrepriya <138755505+matrepriya@users.noreply.github.com>
2023-10-13 22:48:41 +0530

    to_accept_two_no_display.cs

---
+++ c#/to_accept_two_no_display.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace if_else_greater
+{       
+
+    class programe
+    {
+    static void Main(string[] args)
+    {
+        int num1, num2;
+        Console.WriteLine("enter number1 ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("enter number2 ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("enter number1 ");
+        if (num1 > num2)
+            Console.WriteLine("num1 is greater");
+        else
+            Console.WriteLine("num2 is greater");
+        Console.ReadKey();
+     }
+    }
+}
