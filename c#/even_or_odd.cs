19e595b37a3bb18dce2c3d46609a0d9f13847673
matrepriya <138755505+matrepriya@users.noreply.github.com>
2023-10-13 22:05:32 +0530

    even_or_odd.cs

---
+++ c#/even_or_odd.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace evenprogram
+{
+    class even
+    {
+        public static void Main()
+        {
+            int num;
+            Console.WriteLine("enter num");
+            num = Convert.ToInt32(Console.ReadLine());
+            if(num % 2==0)
+            {
+                Console.WriteLine("num is even");
+
+            }
+            else
+            {
+                Console.WriteLine("num is odd");
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
