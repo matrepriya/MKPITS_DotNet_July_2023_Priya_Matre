ab7d7426583cb71af63167150dd5f653a43eea0c
matrepriya <138755505+matrepriya@users.noreply.github.com>
2023-10-13 22:42:59 +0530

    positive_or_negative.cs

---
+++ c#/positive_or_negative.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace programe
+{
+    class programe
+    {
+        static void Main()
+        {
+            int num;
+            Console.WriteLine("enter num");
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num>0)
+            {
+                Console.WriteLine("num is positive interger ");
+            }
+            else if(num==2)
+            {
+                Console.WriteLine("num is negative");
+            }
+            Console.ReadKey();
+        }
+    }
+}
