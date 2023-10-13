5409ee168071bfb1881bd01a8abf7c1242e7e82d
matrepriya <138755505+matrepriya@users.noreply.github.com>
2023-10-13 14:48:44 +0530

    accept_three_no_display_greatest_no.cs

---
+++ c#/accept_three_no_display_greatest_no.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace ifgreaterthreeno
+{
+    class program
+{
+    static void Main(string[]args)
+    {
+        int num1, num2, num3;
+        Console.WriteLine("enter numner 1");
+        num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("enter numner 2");
+        num2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("enter numner 3");
+        num3 = Convert.ToInt32(Console.ReadLine());
+        if (num1 > num2 && num1 > num3)
+            Console.WriteLine("num1 is greater");
+        else if (num2 > num3)
+            Console.WriteLine("num2 is greater");
+        else
+            Console.WriteLine("num3 is grater)");
+        Console.ReadKey();
+        }
+    }
+}
+
