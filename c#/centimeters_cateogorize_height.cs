1ea2f3834b11d18f0940c2d68dda302cea94af66
matrepriya <138755505+matrepriya@users.noreply.github.com>
2023-10-13 22:31:20 +0530

    centimeters_cateogorize_height.cs

---
+++ c#/centimeters_cateogorize_height.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace heightprogram
+{
+    class height
+    {
+        static void Main()
+        {
+            int height;
+            Console.WriteLine("enter tst data");
+            height = Convert.ToInt32(Console.ReadLine());
+            if (height >150)
+            {
+                Console.WriteLine("enter person is tall ");
+            }
+            else
+            {
+                Console.WriteLine("person is DWarf");
+            }
+            Console.ReadKey();
+        }
+        }
+    }
