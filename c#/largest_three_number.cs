14f8bb073dc543cc33245806dca3dd5f2f3098ed
matrepriya <138755505+matrepriya@users.noreply.github.com>
2023-10-14 21:57:27 +0530

    largest_three_number.cs

---
+++ c#/largest_three_number.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace programe
+{
+    class programe
+    {
+        public static void Main()
+        {
+            int num1, num2, num3;
+            Console.WriteLine("intput the 1st number");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("intput the 1st number");
+            num2 = Convert.ToInt32(Console.ReadLine())
+                Console.WriteLine("intput the 1st number");
+            num3 = Convert.ToInt32(Console.ReadLine());
+            if(num1>=num2&&num1<=num3)
+            {
+                Console.WriteLine()
+            }
+        }
+    }
+}
