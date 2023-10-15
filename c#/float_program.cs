23a3619c564c486438f33b00b8860af91a24e85a
matrepriya <138755505+matrepriya@users.noreply.github.com>
2023-10-15 11:59:12 +0530

    float_program.cs

---
+++ c#/float_program.cs
@@ -0,0 +1,16 @@
+﻿ using System;
+ namespace programe
+{
+    class Class1
+    {
+
+        public static void Main()
+        {
+            float b = 123.4567890f; 
+            Console.WriteLine(" value " + b);
+            Console.WriteLine("enter value ");
+            b = Convert.ToSingle(Console.ReadLine());
+            Console.ReadLine();
+        }
+    }
+}   
