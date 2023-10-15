fd92db6c685d963c22876904aa26d91e0fa05ef0
matrepriya <138755505+matrepriya@users.noreply.github.com>
2023-10-15 19:25:03 +0530

    post_program.cs

---
+++ c#/post_program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int j = i++;
+            Console.WriteLine("value of i :" + i + "\n value of j:" + j);
+            Console.ReadKey();
+        }
+    }
+}
