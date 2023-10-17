e837c0623cfd2ec452f6d47b26e379f14ab58924
matrepriya <138755505+matrepriya@users.noreply.github.com>
2023-10-17 16:26:19 +0530

    digit_program.cs

---
+++ c#/digit_program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace digitprogram
+{
+    class program
+    {
+       public static void Main(string[]args)
+        {
+            int digit;
+            Console.WriteLine("enter a digit");
+            digit = Convert.ToInt32(Console.ReadLine());
+            switch(digit)
+            {
+                case 1:
+                    Console.WriteLine("one");
+                    break;
+                case 2:
+                    Console.WriteLine("one");
+                    break;
+                case 3:
+                    Console.WriteLine("one");
+                    break;
+                case 4:
+                    Console.WriteLine("one");
+                    break;
+                case 5:
+                    Console.WriteLine("one");
+                    break;
+                default:
+                    Console.WriteLine();
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
