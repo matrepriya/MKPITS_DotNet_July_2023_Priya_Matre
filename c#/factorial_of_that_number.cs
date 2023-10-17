c4b8d8b0309309f569f73987c2b5e683b5de972e
matrepriya <138755505+matrepriya@users.noreply.github.com>
2023-10-17 21:29:00 +0530

    between_number_betweem_1_to_10.cs         factorial_of_that_number.cs         to_print_dayno.cs

---
+++ c#/factorial_of_that_number.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int fact = 1;
+            Console.WriteLine("enter number");
+            number = Convert.ToInt32(Console.ReadLine()); //5
+            while (number > 0)
+            {
+                fact = fact * number;
+                number--;
+
+            }
+            Console.WriteLine("factorial = " + fact);
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ c#/to_print_dayno.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace dayno
+{
+    class daynumber
+    {
+        public static void Main(string[]args)
+        {
+            int dayno;
+                Console.WriteLine("enter dayno between 1 to 7");
+            dayno = Convert.ToInt32(Console.ReadLine());
+            switch(dayno)
+            {
+                case 1:
+                    Console.WriteLine("monday");
+                    break;
+                case 2:
+                    Console.WriteLine("tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("friday");
+                    break;
+                default:
+                    Console.WriteLine("some other day");
+                    break;
+            }
+            Console.WriteLine();
+        }
+
+        }
+        }
+    
+
