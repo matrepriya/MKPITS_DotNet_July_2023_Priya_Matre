f5d1c048ca454b5fe95eac456c279a6a3a98a4b7
matrepriya <138755505+matrepriya@users.noreply.github.com>
2023-10-20 01:23:55 +0530

    fobnonacci_series_print.cs         inner_outer_program.cs         multiplication_vertically_from_1_to_20.cs

---
+++ c#/fobnonacci_series_print.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace program
+{
+    class program
+    {
+        public static void Main(string[]args)
+        {
+            int num, num1=0,num2 = 1;
+            int add = num1 + num2;
+            int cnt;
+            Console.WriteLine("enter number:");
+            num = Convert.ToInt32(Console.ReadLine());
+     Console.WriteLine("{0},({ 1},",num1,num2);
+            for(cnt=3;cnt<=num;cnt++)
+            {
+                Console.WriteLine("{0},",add);
+                    num1 = num2; 
+                    num2 = add;
+                add = num1 + num2;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ c#/multiplication_vertically_from_1_to_20.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace program
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            int flag;
+            Console.WriteLine("enter limit:");
+            flag = Convert.ToInt32(Console.ReadLine());
+            int counter, cnt1;
+            for (counter = 1; counter <= 10; counter++)
+            {
+                cnt1 = 1;
+                for (cnt1 = 1; cnt1 < flag; cnt1++)
+                {
+                    Console.WriteLine("{o}*{1}={2}", cnt1, counter, (cnt1 * counter);
+                    Console.WriteLine("\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
