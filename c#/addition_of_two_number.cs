374da5a36615f545baad814a64b26126a93121db
matrepriya <138755505+matrepriya@users.noreply.github.com>
2023-10-23 13:41:23 +0530

    addition_of_two_number.cs         circumference_of_circle.cs

---
+++ c#/addition_of_two_number.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace addition_of_two_nmber4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(textBox1.Text);
+            int num2 = Convert.ToInt32(textBox2.Text);
+            int result = num1 + num2;
+            label3.Text = "addition" + result;
+        }
+    }
+}
