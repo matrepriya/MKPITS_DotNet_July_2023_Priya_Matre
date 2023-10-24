73b084e5274163f5d270de4fe2618ce88b1056a5
matrepriya <138755505+matrepriya@users.noreply.github.com>
2023-10-24 20:54:43 +0530

    print_5_to_sum.cs

---
+++ c#/print_5_to_sum.cs
@@ -0,0 +1,30 @@
+﻿using ConsoleApp6;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int sum = 0;
+            for (int cnt = 1; cnt <= 5; cnt++)
+            {
+                Console.WriteLine("enter number ");
+                num = Convert.ToInt32(Console.ReadLine());
+                sum = sum + num;
+            }
+            Console.WriteLine("sum of 5 no.  = " + sum);
+            Console.ReadKey();
+        }
+    }
+}
+
+
