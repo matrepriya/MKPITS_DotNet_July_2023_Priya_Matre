7b7fa964c2f3f3f3fab738a8df82fab1a594f7b9
matrepriya <138755505+matrepriya@users.noreply.github.com>
2023-10-26 23:09:46 +0530

    array_element.cs         two_rows_two_column.cs

---
+++ c#/array_element.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace ConsoleApp8
+{
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            //2 d array of 2 rows and 2 cols
+            int[,] num = { { 1, 2 }, { 3, 4 } };
+            for (int row = 0; row < 2; row++)
+            {
+                for (int col = 0; col < 2; col++)
+                {
+                    Console.Write(num[row, col] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
