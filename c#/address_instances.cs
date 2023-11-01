909ec495b1f3d4705996933bca270b520670f610
matrepriya <138755505+matrepriya@users.noreply.github.com>
2023-11-01 20:58:02 +0530

    address_instances.cs         create_class_libary.cs

---
+++ c#/address_instances.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace program
+{
+    class person
+    {
+        string name;
+        string address;
+        int age;
+        public void getdata(string name, string address, int age)
+        {
+            this.age = age;
+            this.name = name;
+            this.address = address;
+            this.name = name;
+        }
+        public void displaydata()
+        {
+            Console.WriteLine("name:" + name);
+            Console.WriteLine("address:" + address);
+            Console.WriteLine("age:" + age);
+        }
+    }
+    static void Main(string[] args)
+    {
+        person per = new person();
+        Console.WriteLine("enter name:");
+        string n = (Console.ReadLine());
+        Console.WriteLine("enter address:");
+        string age = (Console.ReadLine());
+        Console.WriteLine("enter age");
+        int a = Convert.ToInt32(Console.ReadLine());
+        per.getdata(n, age, a);
+        per.displaydata();
+        Console.ReadKey();
+
+    }
+}
+
+
+
+
