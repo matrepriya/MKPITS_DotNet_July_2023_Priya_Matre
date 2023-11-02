bf8ca854ddc76a4cce0677a7f51303dbaf3ff16a
matrepriya <138755505+matrepriya@users.noreply.github.com>
2023-11-02 21:50:29 +0530

    2d_array.cs

---
+++ c#/2d_array.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace program
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            int[,] arr1 = new int[3, 3];
+            Console.WriteLine("enter no");
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < 3; j++)
+                {
+                    Console.WriteLine(" element {0},{1}", i, j);
+                    arr1[i, j] Convert.ToString(arr1[i, j];
+                }
+            }
+            Console.WriteLine("The matrix is :");
+        }
+        for(i= 0; i<3; i++);
+       {
+            for(j=0; j<3; j++)
+            {
+            Console.WriteLine { {0} \t", arr1 [i,j];
+  }
+    Console.Write("/n /n");
+     }
+    }
+
+
+}
+
+
