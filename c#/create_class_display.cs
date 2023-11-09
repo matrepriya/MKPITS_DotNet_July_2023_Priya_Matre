571b999b913482d0a64559fb06ad12525b99c15c
matrepriya <138755505+matrepriya@users.noreply.github.com>
2023-11-09 22:38:25 +0530

    create_class_display.cs         static_another_student.cs         static_keyword_(static).cs

---
+++ c#/create_class_display.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace program
+{
+    class program
+    {
+        static void display()
+        {
+            Console.WriteLine("hello");
+        }
+        static void Main(string[]args)
+        {
+            display();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ c#/static_another_student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace program
+{ 
+    class student
+    {
+        public static void display ()
+        {
+            Console.WriteLine("hello");
+        }
+        public void shivani()
+        {
+            Console.WriteLine("hi");
+        }
+    }
+    class program
+    {
+        static void Main(string[]args)
+        {
+            student.display();
+            student s = new student();
+            s.shivani();
+        }
+    }
+}
