4a6d080832711de11136e17e6a9d3d88dbe8d980
matrepriya <138755505+matrepriya@users.noreply.github.com>
2023-11-18 15:59:34 +0530

    class_inheritance.cs

---
+++ c#/class_inheritance.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection.Emit;
+
+namespace program
+{
+    class person
+    {
+
+        public string name;
+    }
+    
+    class student : person
+    {
+        public int rno;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            student s = new student();
+            s.rno = 111;
+            s.name = "sachin";
+            Console.WriteLine("rno " + s.rno);
+            Console.WriteLine("name " + s.name);
+        }
+    }
+}
