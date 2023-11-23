a66fbd42be9e9f4ff41fbaf2d80bf581dce60a12
matrepriya <138755505+matrepriya@users.noreply.github.com>
2023-11-23 18:25:29 +0530

    create_student_object.cs

---
+++ c#/create_student_object.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+namespace program
+{
+    class student
+    {
+        public int rno;
+        public string name;
+        public student (int rno,string name)
+        {
+            this.rno = rno;
+            this.name = name;
+        }
+    }
+    class program
+    {
+        static void Main(string[]args)
+        {
+            student s1 = new student(12, "priya");
+            student s2 = new student(14, "sonu");
+            ArrayList a1 = new ArrayList();
+            a1.Add(s1);
+            a1.Add(s2);
+           
+            foreach (student s in a1)
+            {
+                Console.WriteLine("rno" + s.rno);
+                Console.WriteLine("name" + s.name);
+            }    
+        }
+    }
+}
