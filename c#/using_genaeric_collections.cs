d96ca776146faaa6ae5258c277a3321b172c6af2
matrepriya <138755505+matrepriya@users.noreply.github.com>
2023-11-27 21:00:23 +0530

    Dictionary_Tkey_Tvalue.cs         linked_list_program.cs         using_genaeric_collections.cs

---
+++ c#/using_genaeric_collections.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace program
+{
+    class Langs
+    {
+        static void Main(string[]args)
+        {
+            List<string> Langs = new List<string>();
+            Langs.Add("Java");
+            Langs.Add("c#");
+            Langs.Add("c");
+            Langs.Add("c#");
+            Langs.Add("ruby");
+            Langs.Add("Janascript");
+            Console.WriteLine(Langs.Contains("c#"));
+            Console.WriteLine(Langs[1]);
+            Console.WriteLine(Langs[2]);
+            Langs.Remove("c#");
+            Langs.Remove("c");
+            Console.WriteLine(Langs.Contains("c"));
+            Langs.Insert(4, "Haskell");
+            Langs.Sort();
+            foreach (String La in Langs)
+            {
+                Console.WriteLine(La);
+            }
+            Console.ReadKey();
+        }
+    }
+}
