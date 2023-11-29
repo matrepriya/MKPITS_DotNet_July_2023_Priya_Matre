335fcb7b44adb489fd790e203ea6845c22004b24
matrepriya <138755505+matrepriya@users.noreply.github.com>
2023-11-29 20:56:47 +0530

    new file:   Exception_handling.cs         new file:   Try_catch_program.cs

---
+++ c#/Exception_handling.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_handling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 10;
+            int n = 0;
+            int res = num / n;// error will here
+            Console.WriteLine("res=" + res);
+            Console.WriteLine("bye");
+        }
+    }
+}
---
+++ c#/Try_catch_program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Try_catch_program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int n;
+            Console.WriteLine("enter number1");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter number 2");
+            n = Convert.ToInt32(Console.ReadLine());
+            int res = 0;
+            try
+            {
+                res = num / n; // error will come here and program terminates
+            }
+            catch
+            {
+                Console.WriteLine("cannot divided by zero");
+            }
+            Console.WriteLine("res=" + res);
+            Console.WriteLine("bye");
+        }
+    }
+}
+           
+
+            
+                
+            
+    
+
