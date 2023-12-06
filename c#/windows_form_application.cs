3c7fcf4d038c8bf29f93d26661e4ede5ccbe2894
matrepriya <138755505+matrepriya@users.noreply.github.com>
2023-12-06 20:44:41 +0530

    capital_of_windows_form.cs         windows_form_application.cs

---
+++ c#/windows_form_application.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace windows_form_application
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Dictionary<int, Customer> Cust = new Dictionary<int, Customer>();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Customer c1 = new Customer(Convert.ToInt32(textBox1.Text), textBox2.Text);
+            Customer c2 = new Customer(Convert.ToInt32(textBox3.Text), textBox4.Text);
+            Cust.Add(c1.Id, c1);
+            Cust.Add(c2.Id, c2);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach(KeyValuePair <int,Customer>kv in Cust)
+            {
+                sb.Append("Cust id" + kv.Key + "Customer name" + kv.Value.Name + "\n");
+            }
+            label5.Text = sb.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
