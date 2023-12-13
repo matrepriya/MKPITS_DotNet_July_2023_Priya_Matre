3ff875a80af3610e8d7914eea03bd0f69e8e6922
matrepriya <138755505+matrepriya@users.noreply.github.com>
2023-12-13 14:56:44 +0530

    SQLQuery8.sql         countname.sql         firstnameandlastname.sql         firstnamedb.sql         studentdescendingdb.sql

--- sql/SQLQuery2.sql
+++
@@ -1,4 +0,0 @@
-Create  table Student (rno int,name varchar (20));
-insert into Student values (1 , 'priya')
-insert into Student values (2,'vrushali')
-select * From Student
---
+++ sql/firstnamedb.sql
@@ -0,0 +1,8 @@
+create table student (rollno int , firstname varchar(100), lastname varchar (100),city varchar (100), state varchar(100))
+insert into student values (2,'mayuri', 'kale', 'Nagpur','mah')
+insert into student values (2,'prajact', 'matre', 'murtizapur','m.p')
+insert into student values (3,'pooja', 'sangole', 'bander','mah')
+insert into student values (4,'vrushali', 'mohod', 'akola','m.p')
+insert into student values (5,'nikita', 'bharshake', 'panjare','mah')
+insert into student values (6,'suwarana', 'matre', 'akola','m.p')
+select * from student where firstname ='vrushali'
