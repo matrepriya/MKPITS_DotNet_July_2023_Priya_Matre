2b058188b9f3939a7fc265fec904bf3094c25849
matrepriya <138755505+matrepriya@users.noreply.github.com>
2023-12-24 20:13:00 +0530

    employee.sql

---
+++ sql/employee.sql
@@ -0,0 +1,8 @@
+create table employee(empid int primary key identity(1,1),
+                      empname varchar(20))
+
+insert into employee values ('priya')
+insert into employee values ('prachi')
+insert into employee values ('prajact')
+insert into employee values ('priyanka')
+select * from employee
