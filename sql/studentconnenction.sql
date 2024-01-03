c3f3e8fbc6cc7fdafd0bb434cacf43a5e9b1260c
matrepriya <138755505+matrepriya@users.noreply.github.com>
2024-01-03 21:13:25 +0530

    studentconnenction.sql

---
+++ sql/studentconnenction.sql
@@ -0,0 +1,9 @@
+create table student (rollno int primary key,
+                      student varchar (200),
+					  gender varchar (200),
+					  hobby varchar (200),
+					  city varchar (200),
+					  dod varchar (200));
+insert into student values (12,'vrushali','female','music','nagpur','28-01-2000')
+
+select * from student
