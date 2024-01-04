f0b30d1ce692c309b517a9ba1622653df23e60cc
matrepriya <138755505+matrepriya@users.noreply.github.com>
2024-01-04 16:35:26 +0530

    userpassword.cs

---
+++ ado.net/userpassword.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace usernamepassword
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string str = " server=.\\SQLEXPRESS;integrated security = true ;database = passdb";
+        SqlConnection conn = null;
+        SqlCommand cmd = null;
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn = new SqlConnection(str);
+                string qr = "select count(*) from username where username = @username and password = @password";
+                cmd = new SqlCommand(qr, conn);
+                cmd.Parameters.AddWithValue("@username", textBox1.Text);
+                cmd.Parameters.AddWithValue("@password",textBox2.Text);
+                conn.Open();
+                int cnt = Convert.ToInt32(cmd.ExecuteScalar());
+                if (cnt > 0)
+                    label3.Text = "login successfully";
+                Form f = new Form();
+                f.Show();
+                this.Hide();
+            }
+            catch ( Exception ex)
+            {
+                    MessageBox.Show(ex.ToString());
+                }
+             finally
+            {
+                conn.Close();
+            }
+
+
+
+        }
+    
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox1.Focus();
+        }
+    }
+}
