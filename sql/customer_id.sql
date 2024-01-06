a77a1f229ac7aeec757eaee2b78a3148cccac5b1
matrepriya <138755505+matrepriya@users.noreply.github.com>
2024-01-06 19:51:08 +0530

    customer_id.sql         studentdb.sql

---
+++ sql/customer_id.sql
@@ -0,0 +1,16 @@
+create table customers (customer_id int primary key,
+                        custname varchar (500),
+						city varchar (500),
+						phone varchar (500));
+insert into customers(customer_id, custname,city,phone)values
+                     (12,'soap','nagpur','223567'),
+					 (13,'shampoo','akola','233456'),
+					 (14,'lipstick','shegao','123456'),
+					 (15,'leapbom','paras','123457'),
+					 (16,'eyelinear','gaigeao','124567'),
+					 (17,'foundation','mumbai','1234689'),
+					 (18,'nailpolish','pune','234560');
+select * from customers
+
+      
+               
---
+++ sql/studentdb.sql
@@ -0,0 +1,13 @@
+create table student (rollnumber int primary key,
+                     name varchar (500),
+					 class varchar (500),
+					 section varchar (500),
+					 mobile varchar (500));
+insert into student (rollnumber,name,class,section,mobile)values
+                    (12,'priya','a','12335'),
+					(13,'pooja','b','234567'),
+					(14,'priyush','c','123456'),
+					(15,'pragati','d','134678'),
+					(16,'nagesh','a','123789');
+select * from student
+
