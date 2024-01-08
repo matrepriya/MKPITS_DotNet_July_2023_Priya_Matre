b2127809d6821a42daecf4ad3b45ce833e758ca2
matrepriya <138755505+matrepriya@users.noreply.github.com>
2024-01-08 19:28:34 +0530

    createcustomerdb.sql

---
+++ sql/createcustomerdb.sql
@@ -0,0 +1,8 @@
+create table customer (custid int primary key,
+                       custname varchar (500));  
+insert into customer values (1,'priya')
+insert into customer values (2,'pooja')
+insert into customer values (3,'payal');
+select * from customer
+					   
+					   
