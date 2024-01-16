b3e8b3e75076a72d721269901f4297459df39bd0
matrepriya <138755505+matrepriya@users.noreply.github.com>
2024-01-16 19:15:00 +0530

    createtablestateiddb.sql

---
+++ sql/createtablestateiddb.sql
@@ -0,0 +1,6 @@
+create table tablestate (stateid int primary key, nation int )
+insert into tablestate values (12, 12)
+insert into tablestate values (13,14)
+insert into tablestate values (14,18)
+insert into tablestate values (15,17);
+select * from tablestate 
--- sql/tablenationdb.sql
+++ sql/tablenationdb.sql
@@ -1,8 +1,8 @@
-create table tablenation (nationid int primary key,nationname varchar (500))
-insert into tablenation (nationid,nationname)values
-                        (12,'japanes'),
-						(13,'pune'),
-						(14,'mumbai'),
-						(15,'hyderbad'),
-						(16,'indonse');
-select * from tablenation						
+create table Tablenation (nationid int primary key,nationname varchar (500))
+insert into Tablenation values (12,'japan')
+insert into Tablenation values(13,'indonses')
+insert into Tablenation values (14,'bhaire')
+insert into Tablenation values (15,'china')
+insert into Tablenation values (16,'japan')
+insert into Tablenation values (17,'paksitan');
+select * from Tablenation 
