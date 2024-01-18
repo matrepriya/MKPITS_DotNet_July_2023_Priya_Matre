0a02e4525ed7c98d7876a54c174b4120ee6a7503
matrepriya <138755505+matrepriya@users.noreply.github.com>
2024-01-18 14:15:44 +0530

    ../ado.net/fendal_test_project.cs         productsid.sql

---
+++ sql/productsid.sql
@@ -0,0 +1,14 @@
+create table products (products_id int primary key, productname varchar (500))
+insert into products values ( 12,'priya')
+insert into products values (13,'pooja')
+insert into products values (14,'payal')
+insert into products values (15,'pratishka')
+insert into products values(16,'prajanali')
+insert into products values (17,'sonu')
+insert into products values (18,'smaskiks')
+insert into products values (19,'vrushali')
+select * from products
+
+
+
+
